8dca646af56e9e0e306a08b03aee534dcb3d8a29
Daiane Bolzan <dainha85@gmail.com>
2023-10-19 19:05:56 -0300

    fix: alterando diretórios novamente

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,25 +1,26 @@
-export const dataFake=[
+export const dataFake = [
   {
-    "id":"1",
-    "photo":"./../../../assets/images/cristal_e_linda.jpg",
-    "title": "Crystal e Linda dormindo",
-    "description": "O auge da fofura essas duas gatinhas dormindo na minha cama."
+    id: '1',
+    photo: '..//src/assets/images/cristal_e_linda.jpg',
+    title: 'Crystal e Linda dormindo',
+    description: 'O auge da fofura essas duas gatinhas dormindo na minha cama.',
   },
   {
-    "id":"2",
-    "photo":"./../../../assets/images/chico.jpg",
-    "title": "Novo integrante da família está assustado conhecendo a casa nova",
-    "description": "Chico ainda não está familiarizado"
-  },{
-    "id":"3",
-    "photo":"./../../../assets/images/family.jpg",
-    "title": "Linda e Café dormindo no sofá",
-    "description": "Apesar de viverem aos trancos e barrancos eles se amam."
-  },{
-    "id":"4",
-    "photo":"./../../../assets/images/cabeca.jpg",
-    "title": "Cadê a cabeça da Crystal?",
-    "description": "Gatinha dorme com a cabeça escondida."
+    id: '2',
+    photo: '..//src/assets/images/chico.jpg',
+    title: 'Novo integrante da família está assustado conhecendo a casa nova',
+    description: 'Chico ainda não está familiarizado',
   },
-
-]
+  {
+    id: '3',
+    photo: '..//src/assets/images/family.jpg',
+    title: 'Linda e Café dormindo no sofá',
+    description: 'Apesar de viverem aos trancos e barrancos eles se amam.',
+  },
+  {
+    id: '4',
+    photo: '..//src/assets/images/cabeca.jpg',
+    title: 'Cadê a cabeça da Crystal?',
+    description: 'Gatinha dorme com a cabeça escondida.',
+  },
+];
