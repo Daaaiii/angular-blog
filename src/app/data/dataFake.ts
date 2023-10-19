dcd7a530e1d7fbb52822147895c7a1fbe877aa54
Daiane Bolzan <dainha85@gmail.com>
2023-10-19 20:06:23 -0300

    fix: tentativa de solucionar imagens

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,26 +1,27 @@
 export const dataFake = [
   {
     id: '1',
-    photo: '..//src/assets/images/cristal_e_linda.jpg',
+    photo: './src/assets/images/cristal_e_linda.jpg',
     title: 'Crystal e Linda dormindo',
     description: 'O auge da fofura essas duas gatinhas dormindo na minha cama.',
   },
   {
     id: '2',
-    photo: '..//src/assets/images/chico.jpg',
+    photo: './src/assets/images/chico.jpg',
     title: 'Novo integrante da família está assustado conhecendo a casa nova',
     description: 'Chico ainda não está familiarizado',
   },
   {
     id: '3',
-    photo: '..//src/assets/images/family.jpg',
+    photo: './src/assets/images/family.jpg',
     title: 'Linda e Café dormindo no sofá',
     description: 'Apesar de viverem aos trancos e barrancos eles se amam.',
   },
   {
     id: '4',
-    photo: '..//src/assets/images/cabeca.jpg',
+    photo: './src/assets/images/cabeca.jpg',
     title: 'Cadê a cabeça da Crystal?',
     description: 'Gatinha dorme com a cabeça escondida.',
   },
 ];
+
