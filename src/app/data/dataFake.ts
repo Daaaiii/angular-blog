26ade23710750fce6e78864f4099bb88deeba77f
Daiane Bolzan <dainha85@gmail.com>
2023-10-19 20:47:40 -0300

    tentativa #12

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,25 +1,25 @@
 export const dataFake = [
   {
     id: '1',
-    photo: './docs/assets/images/cristal_e_linda.jpg',
+    photo: 'https://github.com/Daaaiii/angular-blog/blob/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/cristal_e_linda.jpg?raw=true',
     title: 'Crystal e Linda dormindo',
     description: 'O auge da fofura essas duas gatinhas dormindo na minha cama.',
   },
   {
     id: '2',
-    photo: './docs/assets/images/chico.jpg',
+    photo: 'https://github.com/Daaaiii/angular-blog/blob/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/chico.jpg?raw=true',
     title: 'Novo integrante da família está assustado conhecendo a casa nova',
     description: 'Chico ainda não está familiarizado',
   },
   {
     id: '3',
-    photo: './docs/assets/images/family.jpg',
+    photo: 'https://github.com/Daaaiii/angular-blog/blob/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/family.jpg?raw=true',
     title: 'Linda e Café dormindo no sofá',
     description: 'Apesar de viverem aos trancos e barrancos eles se amam.',
   },
   {
     id: '4',
-    photo: './docs/assets/images/cabeca.jpg',
+    photo: 'https://raw.githubusercontent.com/Daaaiii/angular-blog/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/cabeca.jpg',
     title: 'Cadê a cabeça da Crystal?',
     description: 'Gatinha dorme com a cabeça escondida.',
   },
