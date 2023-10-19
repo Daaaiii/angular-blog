17fdc632c1909219551d5f00f439484aa376fc31
Daiane Bolzan <dainha85@gmail.com>
2023-10-19 20:53:07 -0300

    Tentativa #13

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,19 +1,19 @@
 export const dataFake = [
   {
     id: '1',
-    photo: 'https://github.com/Daaaiii/angular-blog/blob/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/cristal_e_linda.jpg?raw=true',
+    photo: 'assets/images/cristal_e_linda.jpg',
     title: 'Crystal e Linda dormindo',
     description: 'O auge da fofura essas duas gatinhas dormindo na minha cama.',
   },
   {
     id: '2',
-    photo: 'https://github.com/Daaaiii/angular-blog/blob/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/chico.jpg?raw=true',
+    photo: 'assets/images/chico.jpg',
     title: 'Novo integrante da família está assustado conhecendo a casa nova',
     description: 'Chico ainda não está familiarizado',
   },
   {
     id: '3',
-    photo: 'https://github.com/Daaaiii/angular-blog/blob/815a46a4dfa397be97af90fe556fb8bfd573b018/src/assets/images/family.jpg?raw=true',
+    photo: 'assets/images/family.jpg',
     title: 'Linda e Café dormindo no sofá',
     description: 'Apesar de viverem aos trancos e barrancos eles se amam.',
   },
